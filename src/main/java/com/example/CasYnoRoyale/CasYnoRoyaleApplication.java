9ada313ed5e763aa6ce3f516abf88b6a96c24a9c
Mathis <mathis.balet@gmail.com>
2025-11-21 14:44:26 +0100

    Début projet SpringBoot

---
+++ src/main/java/com/example/CasYnoRoyale/CasYnoRoyaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.CasYnoRoyale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasYnoRoyaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasYnoRoyaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CasYnoRoyale/CasYnoRoyaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CasYnoRoyale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasYnoRoyaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
