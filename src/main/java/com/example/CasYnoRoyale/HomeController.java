3d7d54011d281d1749d2c328665dbcf4fe4ad21a
Guillaume Arricastres <garricastres@gmail.com>
2025-11-28 13:55:59 +0100

    ajout du bouton de connexion

--- src/main/java/com/example/CasYnoRoyale/HomeController.java
+++ src/main/java/com/example/CasYnoRoyale/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
@@ -19,5 +20,14 @@ public class HomeController {
         }
         return "index";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(@ModelAttribute AppUser formUser)
+
 
 }
