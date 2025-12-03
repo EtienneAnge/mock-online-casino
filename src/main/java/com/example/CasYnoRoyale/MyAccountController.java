32b4b5b31bb19fa6fc9d3e68c1c57e3929b2d884
Guillaume Arricastres <garricastres@gmail.com>
2025-12-03 20:53:51 +0100

    ajout de la page du compte. Possibilité de modifier son nom, prenom, mot de passe

---
+++ src/main/java/com/example/CasYnoRoyale/MyAccountController.java
@@ -0,0 +1,113 @@
+package com.example.CasYnoRoyale;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.CasYnoRoyale.database.AppUser;
+import com.example.CasYnoRoyale.repository.AppUserRepository;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class MyAccountController {
+
+    @Autowired
+    AppUserRepository userRepository;
+
+    @GetMapping("/myaccount")
+    public String myaccount() {
+        return "myaccount";
+    }
+
+
+    @PostMapping("/myaccount/update-name")
+    public String updateName(@RequestParam("newName") String newName,
+                            RedirectAttributes model,
+                            HttpSession session) {
+
+        AppUser user = (AppUser) session.getAttribute("user");
+        
+        if (user == null) {
+            return "redirect:/login";
+        }
+
+        user.setName(newName);
+
+        userRepository.save(user);
+
+        model.addFlashAttribute("message", "Nom mis à jour !");
+        return "redirect:/myaccount";
+    }
+
+    @PostMapping("/myaccount/update-username")
+    public String updateUsername(@RequestParam("newUsername") String newUsername,
+                            RedirectAttributes model,
+                            HttpSession session) {
+
+        AppUser user = (AppUser) session.getAttribute("user");
+        
+        if (user == null) {
+            return "redirect:/login";
+        }
+
+        user.setUsername(newUsername);
+
+        userRepository.save(user);
+
+        model.addFlashAttribute("message", "Pseudo mis à jour !");
+        return "redirect:/myaccount";
+    }
+
+    @PostMapping("/myaccount/update-password")
+    public String updatePassword(@RequestParam("newPassword") String newPassword,
+                            @RequestParam("oldPassword") String oldPassword,
+                            RedirectAttributes model,
+                            HttpSession session) {
+
+        AppUser user = (AppUser) session.getAttribute("user");
+        
+        if (user == null) {
+            return "redirect:/login";
+        }
+
+        if (!user.getPassword().equals(oldPassword)) {
+            model.addFlashAttribute("error", "Mot de passe invalide");
+            return "redirect:/myaccount";
+        }
+
+        user.setName(newPassword);
+
+        userRepository.save(user);
+
+        model.addFlashAttribute("message", "Mot de passe mis à jour !");
+        return "redirect:/myaccount";
+    }
+
+    @PostMapping("/myaccount/add-balance")
+    public String addBalance(RedirectAttributes model,
+                            HttpSession session) {
+        AppUser user = (AppUser) session.getAttribute("user");
+        BigDecimal maxBalance = new BigDecimal(100.0);
+        
+        if (user == null) {
+            return "redirect:/login";
+        }
+
+        if (user.getBalance().compareTo(maxBalance) >= 0) {
+            model.addFlashAttribute("error", "Vous avez déjà 100 de crédits. Dépensez les !");
+        } else {
+            user.setBalance(maxBalance);
+            userRepository.save(user);
+
+            model.addFlashAttribute("message", "Solde mis à jour !");
+        }
+
+        return "redirect:/myaccount";
+    }
+}
