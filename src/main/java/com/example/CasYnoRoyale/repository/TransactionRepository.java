ca1ab20ec8f7e60a66583b05cab634875bc882b9
Guillaume Arricastres <garricastres@gmail.com>
2025-12-05 17:48:13 +0100

    ajout de la recuperation des transactions depuis la bdd

--- src/main/java/com/example/CasYnoRoyale/repository/TransactionRepository.java
+++ src/main/java/com/example/CasYnoRoyale/repository/TransactionRepository.java
@@ -3,9 +3,22 @@ package com.example.CasYnoRoyale.repository;
 
 
 import com.example.CasYnoRoyale.database.Transaction;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    @Query("SELECT t FROM Transaction t " + 
+       "JOIN t.room r " + 
+       "JOIN r.users u " + 
+       "WHERE u.idUser = :userId AND r.game.idGame IN :gameIds")
+List<Transaction> findTransactionsByUserAndGames(
+    @Param("userId") Long userId, 
+    @Param("gameIds") List<Long> gameIds
+);
 }
